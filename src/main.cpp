3629ac06de4f2434f19aeb95d1240dbbcac9f35c
MahmoudHamdy02 <mahmoud-mahgoub1@hotmail.com>
2025-01-30 21:02:18 +0200

    initial commit: empty window with bg color

---
+++ src/main.cpp
@@ -0,0 +1,66 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+// Resize viewport when window size changes
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+// Handle input
+void processInput(GLFWwindow *window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+int main() {
+    // Initialize GLFW and specify version
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Make a window object and set the current context to it
+    GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // Use GLAD to link OS-specific function pointers
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // Specify openGL viewport size
+    glViewport(0, 0, 800, 600);
+
+    // Register window size callback
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // Render loop
+    while (!glfwWindowShouldClose(window)) {
+        // Process input
+        processInput(window);
+
+        // Main rendering code goes here
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Render color buffers
+        glfwSwapBuffers(window);
+
+        // Poll for events such as keyboard or mice
+        glfwPollEvents();
+    }
+
+    // Exit cleanly
+    glfwTerminate();
+
+    return 0;
+}
