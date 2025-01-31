4a19e9315c7ad73f8acab92c329a86cc6ebbe7d2
MahmoudHamdy02 <mahmoud-mahgoub1@hotmail.com>
2025-01-31 22:37:18 +0200

    use EBO, refactor

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,12 @@
 
 // OpenGL doesn't have default vertex or fragment shaders
 // Vertex shader defined here for now
-const char *vertexShaderSource =
-    "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "void main()\n"
-    "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-    "}\0";
+const char *vertexShaderSource = "#version 330 core\n"
+                                 "layout (location = 0) in vec3 aPos;\n"
+                                 "void main()\n"
+                                 "{\n"
+                                 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                                 "}\0";
 
 // Fragment (color) shader
 const char *fragmentShaderSource = "#version 330 core\n"
@@ -22,9 +21,7 @@ const char *fragmentShaderSource = "#version 330 core\n"
                                    "}\0";
 
 // Resize viewport when window size changes
-void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
-    glViewport(0, 0, width, height);
-}
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) { glViewport(0, 0, width, height); }
 
 // Handle input
 void processInput(GLFWwindow *window) {
@@ -34,6 +31,7 @@ void processInput(GLFWwindow *window) {
 }
 
 int main() {
+    // --------------------- Initalization ---------------------
     // Initialize GLFW and specify version
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -61,36 +59,10 @@ int main() {
     // Register window size callback
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    // Render a simple triangle
-    // Define vertices
-    float vertices[] = {-0.5f, -0.5f, 0.0f, 0.5f, -0.5f,
-                        0.0f,  0.0f,  0.5f, 0.0f};
-
-    // Define a VAO object to hold attribute data, and bind it
-    unsigned int VAO;
-    glGenVertexArrays(1, &VAO);
-    glBindVertexArray(VAO);
-
-    // A buffer in OpenGL is an object that manages a certain piece of GPU
-    // memory and nothing more. We give meaning to a buffer when binding it to a
-    // specific buffer target
-
-    // Define and bind vertex buffer object
-    unsigned int VBO;
-    // Generate 1 buffer, and assign its ID to VBO
-    glGenBuffers(1, &VBO);
-
-    // Bind VBO to an array buffer to hold vertex data
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
-    // Copy vertex data to array buffer. Size is defined in bytes
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices,
-                 GL_STATIC_DRAW); // This operates on VBO
-
-    // We need to specify how the vertex data should be interpreted
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
-                          (void *)0);
-    glEnableVertexAttribArray(0);
+    // --------------------- Shaders ---------------------
+    // Variables for checking compilation status
+    int success;
+    char infoLog[512];
 
     // 1- Vertex shader
     // Create shader object
@@ -100,16 +72,11 @@ int main() {
     glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
     glCompileShader(vertexShader);
 
-    // Variables for checking compilation status
-    int success;
-    char infoLog[512];
-
     // Check if shader was compiled correctly
     glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
     if (!success) {
         glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
-                  << infoLog << std::endl;
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
     }
 
     // 2- Fragment shader
@@ -121,8 +88,7 @@ int main() {
     glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
     if (!success) {
         glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n"
-                  << infoLog << std::endl;
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
     }
 
     // Now that both shaders are compiled, we need to link them in a shader
@@ -137,29 +103,62 @@ int main() {
     glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
     if (!success) {
         glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::PROGRAM::COMPILATION_FAILED\n"
-                  << infoLog << std::endl;
+        std::cout << "ERROR::SHADER::PROGRAM::COMPILATION_FAILED\n" << infoLog << std::endl;
     }
 
     // Delete shader objects as they are not needed anymore
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
+    // --------------------- Shape setup ---------------------
+    // Define vertices
+    float vertices[] = {
+        0.5f, 0.5f, 0.0f, 
+        -0.5f, -0.5f, 0.0f, 
+        -0.5f, 0.5f, 0.0f, 
+        0.5f, -0.5f, 0.0f
+    };
+
+    // Define indices
+    unsigned int indices[] = {
+        0, 1, 2, 
+        0, 1, 3
+    };
+
+    // Define objects to hold the data and attributes
+    unsigned int VAO, VBO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+    // Bind the VAO, and store the following VBO and EBO
+    glBindVertexArray(VAO);
+
+    // Bind the buffer and load the vertices data
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); // This operates on VBO
+    // The same for EBO
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // We need to specify how the vertex data should be interpreted
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+
     // Render loop
     while (!glfwWindowShouldClose(window)) {
         // Process input
         processInput(window);
 
-        // Main rendering code goes here
+        // Set background color
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         // Activate shader program
         glUseProgram(shaderProgram);
-        // Bind VAO
+        // Bind VAO and draw the shape
         glBindVertexArray(VAO);
-        // Draw the vertices
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
 
         // Render color buffers
         glfwSwapBuffers(window);
