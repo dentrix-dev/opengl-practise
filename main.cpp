db5c1fee7fadca342ceea842a58698323f0fe161
mohamedAdhamc <mohamed.adhamnagiub@gmail.com>
2025-02-11 22:24:00 +0200

    finish helllo window

--- main.cpp
+++ main.cpp
@@ -7,11 +7,20 @@ using namespace std;
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
+    cout<<"resize callback called"<<endl;
+}
+
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
 }
 
 int main()
 {
+    //init glfw
     glfwInit();
+    //configure various options of GLFW, (option, value)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
@@ -24,19 +33,31 @@ int main()
         return -1;
     }
 
-    glfwMakeContextCurrent(window); // ✅ Move this before loading GLAD
+    glfwMakeContextCurrent(window);
 
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) // ✅ Load GLAD *after* OpenGL context is created
+    //init glad
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
 
+    //tell opengl the size of the rendering- first 2 are lower left window corner points, then width x height
     glViewport(0, 0, 800, 600);
+
+    //register callback for window resize -- in general as is usual, register the callbacks of the window before the render loop
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     while (!glfwWindowShouldClose(window))
     {
+        //check for escape press (and other keys in general)
+        processInput(window);
+        //clear the previous frame
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);//state setting function
+        glClear(GL_COLOR_BUFFER_BIT);//state-using function, uses curr state to retrieve clearing color from
+        //insert rendering commands
+
+        //swap the two color buffers associated with the window
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
